2f5e047873ea89d8a7b02e5b81e4441c5c612025
Simo Ndaoud <ndaoud@MohamedaoudsMBP.attlocal.net>
2021-02-18 14:50:11 -0800

    constants modified

--- constants.py
+++ constants.py
@@ -1,14 +1,19 @@
 import numpy as np
 """Store some constant variable.
 """
-STEPS = 5  # number of experiments
+STEPS = 20  # number of experiments
 N_ITERATION = 200  # number of iterations in ISTA or Hard Threshold
-N, P, S = 50, 200, 10
-X_VALUE = 1.  # true signal
+N, P, S = 200, 1000, 10
+
 temp = np.ones((P))
 SIGMA_COVAR_MATRIX_HALF = {"isotropic": np.diag(temp)}
 temp[P // 2:] = 10
 #temp[:p // 2] = 10
 temp = np.random.permutation(temp)
 SIGMA_COVAR_MATRIX_HALF["anisotropic"] = np.diag(temp)  # half of design covariance
-SIGMA_NUMBER = 0.1
+SIGMA_NUMBER = .1
+X_VALUE = 1.  # true signal
+X = X_VALUE * np.ones((P,1))
+#x[:p-s] = 0
+X[S:] = 0
+X = np.random.permutation(X)

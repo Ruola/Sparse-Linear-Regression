684a66aab8b605d7f548e5fe6d7c8009ab13426b
Ruola <>
2021-03-07 00:08:43 -0800

    add fast newton

--- compare_gradient_descent.py
+++ compare_gradient_descent.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 import numpy as np
 import os
 
+from algorithms.fast_newton import FastNewton
 from algorithms.gradient_descent import GradientDescent
 from algorithms.iterative_threshold_methods import IterativeThresholdMethods
 import utils.constants as constants
@@ -15,12 +16,18 @@ from utils.generate_data import GenerateData
 class Compare:
     """Do a simulation to compare gradient descent methods with IHT / HTP.
     """
-    def __init__(self, design=constants.ANISOTROPIC_NAME):
+    def __init__(self, design, gd_types, thres_types, other_methods):
         """Set the type of design matrix, e.g. isotropic or anisotropic.
 
         @param design - the type of design matrix, e.g. isotropic or anisotropic.
+        @param gd_types - GD, NGD, or Newton.
+        @param thres_types - IHT or HTP.
+        @param other_methods - ISTA or Fast Newton.
         """
         self.design = design
+        self.gd_types = gd_types
+        self.thres_types = thres_types
+        self.other_methods = other_methods
         self.steps = constants.GD_STEPS  # number of experiments
         self.num_iter = constants.GD_NUM_ITERATION  # number of iterations
         self.x_original = constants.X
@@ -43,19 +50,18 @@ class Compare:
         plt.ylabel("validation error from cv")
         plt.legend()
 
-    def compare_validation_error(self, gd_types, thres_types):
+    def compare_validation_error(self):
         """Get the change of testing error w.r.t. (final) threshold in one experiment.
-        
-        @param design: "isotropic" or "anisotropic".
         """
         y, H, self.SIGMA_half = GenerateData(self.design).generate_data()
-        for gd_type in gd_types:
-            for thres_type in thres_types:
+        for gd_type in self.gd_types:
+            for thres_type in self.thres_types:
                 _, best_lambda, gener_error, map_of_thres_error = GradientDescent(
                 ).get_errors_by_cv(self.x_original, y, H, self.num_iter,
-                                   self.SIGMA_half, gd_type, thres_type, True)
+                                   self.SIGMA_half, self.gd_type,
+                                   self.thres_type, True)
                 self.draw_change_of_error_by_threshold(
-                    map_of_thres_error, gd_type + " + " + thres_type)
+                    map_of_thres_error, self.gd_type + " + " + self.thres_type)
         plt.savefig(
             os.path.dirname(os.path.abspath(__file__)) +
             "/figures/second order methods/error by threshold " + self.design)
@@ -77,9 +83,20 @@ class Compare:
                                    self.SIGMA_half, gd_type, thres_type, False)
                 algo_error_map[algo_name] = gener_error
                 print(gd_type, thres_type, best_lambda)
+        for algo_name in self.other_methods:
+            if algo_name == constants.FAST_NEWTON_NAME:
+                _, _, gener_error = FastNewton().get_errors_by_cv(
+                    self.x_original, y, H, self.num_iter, self.SIGMA_half,
+                    constants.FAST_NEWTON_NUM_GD, False)
+            else:
+                _, _, gener_error = IterativeThresholdMethods(
+                ).get_errors_by_cv(self.x_original, y, H, self.num_iter,
+                                   self.SIGMA_half, algo_name, False)
+            algo_error_map[algo_name] = gener_error
+            print(algo_name, " error ", gener_error[-1])
         return algo_error_map
 
-    def compare_gradient_descent(self, gd_types, thres_types):
+    def compare_gradient_descent(self):
         """Compare gradient descent, natural gd, newton with IHT / HTP.
         """
         gener_errors_matrix_map = dict()
@@ -90,24 +107,25 @@ class Compare:
         """
         algo_gener_errors_map = dict()
         # Set initial value of gener_errors_matrix_map
-        for gd_type in gd_types:
-            for thres_type in thres_types:
+        for gd_type in self.gd_types:
+            for thres_type in self.thres_types:
                 algo_name = gd_type + "+" + thres_type
                 algo_gener_errors_map[algo_name] = np.zeros((self.num_iter))
+        for algo_name in other_methods:
+            algo_gener_errors_map[algo_name] = np.zeros((self.num_iter))
 
         # To multiprocess several experiments.
-        self.gd_types = gd_types
-        self.thres_types = thres_types
         pool = mp.Pool(mp.cpu_count())
-        pool_result = pool.map(self.run_one_experiment, [1] * self.steps, chunksize=1)
+        pool_result = pool.map(self.run_one_experiment, [1] * self.steps,
+                               chunksize=1)
         for algo_error_map in pool_result:
             for algo_name in algo_error_map:
                 algo_gener_errors_map[algo_name] += algo_error_map[algo_name]
-        for algo_name in algo_gener_errors_map:
+        for algo_name in algo_gener_errors_map:  # take average
             algo_gener_errors_map[algo_name] /= self.steps
 
-        for algo_name in algo_gener_errors_map:
-                plt.plot(algo_gener_errors_map[algo_name], label=algo_name)
+        for algo_name in algo_gener_errors_map:  # plot
+            plt.plot(algo_gener_errors_map[algo_name], label=algo_name)
         plt.title("Second order methods comparison in " + self.design +
                   " design")
         plt.xlabel("#iterations")
@@ -132,14 +150,18 @@ if __name__ == "__main__":
     # Change gd_types and thres_types according to the needs.
     gd_types = (constants.GD_NAME, constants.NEWTON_NAME)
     thres_types = (constants.IHT_NAME, constants.HTP_NAME)
+    other_methods = (constants.FAST_NEWTON_NAME, )
+
+    isotropic_obj = Compare(constants.ISOTROPIC_NAME, gd_types, thres_types,
+                            other_methods)
+    anisotropic_obj = Compare(constants.ANISOTROPIC_NAME, gd_types,
+                              thres_types, other_methods)
     """Draw the change of testing error w.r.t. (final) threshold.
         Please comment these two lines out if you do not need it.
     """
-    #Compare(constants.ISOTROPIC_NAME).compare_validation_error(gd_types, thres_types)
-    #Compare(constants.ANISOTROPIC_NAME).compare_validation_error(gd_types, thres_types)
+    #isotropic_obj.compare_validation_error()
+    #anisotropic_obj.compare_validation_error()
     """Draw the change of generalization error w.r.t. iterations.
     """
-    Compare(constants.ISOTROPIC_NAME).compare_gradient_descent(
-        gd_types, thres_types)
-    Compare(constants.ANISOTROPIC_NAME).compare_gradient_descent(
-        gd_types, thres_types)
+    isotropic_obj.compare_gradient_descent()
+    anisotropic_obj.compare_gradient_descent()
